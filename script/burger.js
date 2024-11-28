d5542e22f2ee7426f042dc9c24fa51cc7a449f76
Nikita  Mir <166736167+NekitMir@users.noreply.github.com>
2024-11-28 20:30:20 +0300

    metrica-final

---
+++ script/burger.js
@@ -0,0 +1,30 @@
+const BURGER_OPENED_CLASSNAME = 'burger_open'
+const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open'
+// const BODY_FIXED_CLASSNAME = 'body_fixed'
+
+
+// const bodyNode = document.querySelector('body')
+const burgerNode = document.querySelector('.js-burger')
+const burgerBtnNode = document.querySelector('.js-burger-btn')
+const burgerContentNode = document.querySelector('.js-burger__content')
+// const burgerOpenClick = document.querySelector('.js-burger-click')
+//
+// burgerNode.addEventListener('click', () => {
+//     console.log('burger')
+// })
+
+burgerBtnNode.addEventListener('click', toggleBurger);
+
+burgerNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+
+    if (isClickOutsideContent) {
+        toggleBurger();
+    }
+})
+
+function toggleBurger() {
+    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+}
